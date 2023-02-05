7ddcc0f8344dbca756ab0d8596b6867af4fca374
shrikantarchit <109203106+shrikantarchit@users.noreply.github.com>
2023-02-05 18:19:45 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,16 @@
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class HelloWorld{
 
   public static void main(String[] args) {
-        System.out.println("Hello, World!"); 
-    }
+        System.out.println("Hello, World!");
+    String id = "admin"; // hard-coded credential
+    String password = "password"; // hard-coded credential
+    getConn(id,password);
+}
+@GetMapping("/api")
+public static void getConn(String id, String password) throws SQLException {
+    DriverManager.getConnection(id, password); // sensitive call
+}
 }
