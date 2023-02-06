15a0615f56e870630c87c6f89663fed86fa72a76
shrikantarchit <109203106+shrikantarchit@users.noreply.github.com>
2023-02-06 08:31:25 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -5,12 +5,21 @@ public class HelloWorld{
 
   public static void main(String[] args) {
         System.out.println("Hello, World!");
-    String id = "admin"; // hard-coded credential
-    String password = "password"; // hard-coded credential
-    getConn(id,password);
+        String id = "admin"; // hard-coded credential
+        String password = "password"; // hard-coded credential
+       Boolean check= VerifyAdmin(password);
+        getConn(id,password);
 }
 @GetMapping("/api")
 public static void getConn(String id, String password) throws SQLException {
     DriverManager.getConnection(id, password); // sensitive call
 }
+  public boolean VerifyAdmin(String password) {
+if (password.equals("68af404b513073584c4b6f22b6c63e6b")) {
+System.out.println("Entering Diagnostic Mode...");
+return true;
+}
+System.out.println("Incorrect Password!");
+return false;
+  }
 }
