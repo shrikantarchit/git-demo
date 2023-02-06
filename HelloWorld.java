786caad0c6b5ec4fe9b0befb2bda54d499b2ebff
shrikantarchit <109203106+shrikantarchit@users.noreply.github.com>
2023-02-06 21:19:58 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -7,7 +7,7 @@ public class HelloWorld{
         System.out.println("Hello, World!");
         String id = "admin"; // hard-coded credential
         String password = "password"; // hard-coded credential
-       Boolean check= VerifyAdmin(password);
+       Boolean check= VerifyAdmin("password");
         //getConn(id,password);
 }
 //@GetMapping("/api")
@@ -18,7 +18,7 @@ public static void getConn(String id, String password) throws SQLException {
   JavaCharArray[2] = 'c';  
   JavaCharArray[3] = 'd';  
   JavaCharArray[4] = 'e';  
-    DriverManager.getConnection(id, password,password); // sensitive call
+    DriverManager.getConnection(id, "password",password); // sensitive call
 }
   public static boolean VerifyAdmin(String password) {
 if (password.equals("68af404b513073584c4b6f22b6c63e6b")) {
